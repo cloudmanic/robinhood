3fcb58722d7fae2ac87101a46ae32d6db821bac2
Spicer Matthews <spicer@cloudmanic.com>
2015-06-04 16:33:14 -0700

    Added the example back in

--- example.php
+++ example.php
@@ -1,7 +1,24 @@
 <?php
-  require_once __DIR__ . '/../vendor/autoload.php';
-  include 'RobinHoodObj.php';
+  use Cloudmanic\RobinHood\Api;
   
-  $rh = new RobinHoodObj();
+  // Auth and get a token
+  $token = API::auth('Your Username', 'Your Password');
   
-  $rh->auth('your username here', 'your password here');
+  // Or we already have a token from a past auth and we just set the token.
+  // Api::set_token($token);
+  
+  // Get all accounts.
+  $acts = Api::get_accounts();
+  echo '<pre>' . print_r($acts, TRUE) . '</pre>';
+  
+  // Get one account from the account url.
+  $act = Api::get_account($acts['results'][0]['url']);
+  echo '<pre>' . print_r($act, TRUE) . '</pre>';
+  
+  // Get the summery of an account.
+  $port = Api::get_portfolio_summery($acts['results'][0]['portfolio']);
+  echo '<pre>' . print_r($port, TRUE) . '</pre>';
+  
+  // Get current positions from the account.
+  $pos = Api::get_current_postions($acts['results'][0]['positions']);
+  echo '<pre>' . print_r($pos, TRUE) . '</pre>';
